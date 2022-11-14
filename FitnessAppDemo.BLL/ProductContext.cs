e62ae1b4f9e932ad190b9743dd204d17ecf319d8
Eugene Patysh <eugene.patysh@gmail.com>
2022-11-14 19:04:47 +0300

    Created Project

---
+++ FitnessAppDemo.BLL/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FitnessAppDemo.BLL
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        // MOVE TO PROGRAM
+        // Connection to PostgreSql DataBase
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     // use postgreSQL
+        //     optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=organization;Username=postgres;Password=123");
+        // }
+    }
+}
